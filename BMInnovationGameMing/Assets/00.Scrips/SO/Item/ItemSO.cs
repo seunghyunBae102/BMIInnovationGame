821e8e5b7ffb4336ccca3ceb891a40d23005b404
unknown <hydrogenggms@gmail.com>
2025-10-17 10:32:13 +0900

    FolderSet

--- BMInnovationGameMing/Assets/00.Scrips/SO/Item/ItemSO.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ItemSO", menuName = "SO/ItemSO")]
-public class ItemSO : ScriptableObject
-{
-    public Sprite visual;
-}
--- BMInnovationGameMing/Assets/00.Scrips/SO/MapGenSourceSO.cs
+++
@@ -1,9 +0,0 @@
-
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "MapGenSource", menuName = "SO/MapGenSource")]
-public class MapGenSourceSO : ScriptableObject
-{
-    public List<BashList<InsideArray<ItemSO>>> map;
-}
